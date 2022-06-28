dd4fef8757557ce6f76dfbd79a88a17fec4ad19e
mkaruc <fatihmehmetkaruc@gmail.com>
2022-06-28 15:05:48 +0300

    endOfTheProject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import languages from './helper/data'
+import Card from './components/card/Card';
+import reactLogo from './assets/react.svg';
+import './App.module.css';
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <header>
+        <div className='reactLogo'>
+          <img src={reactLogo} alt="" />
+        </div>
+      </header>
+      <main className='mainPanel'>
+      <h1>Languages</h1>
+      <article>
+        {languages.map((item, index) => {
+          return (
+            <Card data={item} key={index}/>
+          )
+        })}</article>
+      </main>
+    </div>
   )
 }
 
